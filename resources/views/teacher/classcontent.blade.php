5829d16f2eee4b661074e551c2fd90d99d76a4de
Paul-Ben <paulben.ajene@gmail.com>
2024-08-14 21:41:52 +0100

    Minor updates

--- resources/views/teacher/classcontent.blade.php
+++ resources/views/teacher/classcontent.blade.php
@@ -68,7 +68,7 @@
                                         </svg> 
                                     </a>
                                 @else
-                                    <a href="{{ asset($content->link) }}" target="blank">
+                                    <a href="{{ asset($content->external_link) }}" target="blank">
                                         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"/>
                                             <h3>External link</h3> 
@@ -121,7 +121,7 @@
                                         @csrf
                                         @method('DELETE')
                                         <div class="flex">
-                                            <a href="{{ asset($content->link) }}" target="blank"
+                                            <a href="{{ asset($content->external_link) }}" target="blank"
                                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 View
                                             </a>

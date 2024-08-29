22b0b94e7eaf886c0c20360977b6ac034cc1a376
Paul-Ben Ajene <92761584+Paul-Ben@users.noreply.github.com>
2024-08-29 18:11:54 +0100

    Update dashboard.blade.php

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -45,7 +45,7 @@
                         <th scope="row"
                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             @if (isset($content->video))
-                                <a href="{{ asset($content->video) }}" target="blank">
+                                <a href="{{ asset('/storage/'.$content->video) }}" target="blank">
                                     
                                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M14 7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7Zm2 9.387 4.684 1.562A1 1 0 0 0 22 17V7a1 1 0 0 0-1.316-.949L16 7.613v8.774Z" clip-rule="evenodd"/>
@@ -53,7 +53,7 @@
                                     </svg>                                          
                                 </a>
                             @elseif (isset($content->document))
-                                <a href="{{ asset($content->document) }}" target="blank">
+                                <a href="{{ asset('/storage/'.$content->document) }}" target="blank">
                                     
                                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M9 2.221V7H4.221a2 2 0 0 1 .365-.5L8.5 2.586A2 2 0 0 1 9 2.22ZM11 2v5a2 2 0 0 1-2 2H4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2 2 2 0 0 0 2 2h12a2 2 0 0 0 2-2 2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2V4a2 2 0 0 0-2-2h-7Zm-6 9a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0v-1h.5a2.5 2.5 0 0 0 0-5H5Zm1.5 3H6v-1h.5a.5.5 0 0 1 0 1Zm4.5-3a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h1.376A2.626 2.626 0 0 0 15 15.375v-1.75A2.626 2.626 0 0 0 12.375 11H11Zm1 5v-3h.375a.626.626 0 0 1 .625.626v1.748a.625.625 0 0 1-.626.626H12Zm5-5a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0v-1h1a1 1 0 1 0 0-2h-1v-1h1a1 1 0 1 0 0-2h-2Z" clip-rule="evenodd"/>
@@ -83,14 +83,14 @@
                         <td class="px-6 py-4">
                             @if (isset($content->video))
                                     <div class="flex">
-                                        <a href="{{ asset($content->video) }}" target="blank"
+                                        <a href="{{ asset('/storage/'.$content->video) }}" target="blank"
                                             class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             View
                                         </a>
                                     </div>
                             @elseif (isset($content->document))
                                     <div class="flex">
-                                        <a href="{{ asset($content->document) }}" target="blank"
+                                        <a href="{{ asset('/storage/'.$content->document) }}" target="blank"
                                             class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             View
                                         </a>
